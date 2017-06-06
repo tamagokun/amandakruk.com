ab2c1ce2d96b83f7cc0f922df5a16f4a24052822
Mike Kruk <tamagokun@gmail.com>
2017-06-06 13:58:58 -0400

    content!

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,39 @@ export default () => <div>
       body {
         background: url('/static/amandakruk.jpg') no-repeat center top;
         background-size: cover;
+        color: white;
+        font-family: Helvetica, sans-serif;
+        font-size: 150%;
+        font-weight: 100;
+        line-height: 1.3;
+      }
+
+      main {
+        padding: 15%;
+        max-width: 450px;
+      }
+
+      h1 {
+        font-size: 1.25em;
+        font-weight: 400;
+      }
+
+      a:link, a:visited {
+        color: white;
+        border-bottom: 1px solid;
+        text-decoration: none;
+      }
+
+      a:hover {
+        color: #d6d6d6;
       }
     `}</style>
   </Head>
+  <main>
+    <h1>Amanda Kruk</h1>
+    <p>Classically trained Mezzo-Soprano for Wedding Ceremonies, Baptisms, Anniversaries and Events.</p>
+    <p>
+      <a href='mailto:info@amandakruk.com'>info@amandakruk.com</a>
+    </p>
+  </main>
 </div>
